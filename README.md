10c3da931c585967dcd8ad3d6e8459ec31466ebf
fgarsombke <franz@rachio.com>
2016-10-20 19:33:17 +0000

    first

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# node-mock-webhook
+node-mock-webhook
+=================
+Used with ngrok to standup an external service that can be used as a mock webhook endpoint.
+
+start ngrok
+
+./ngrok 8081
+
+start node
+
+node app.js
+
+Use the endpoint as a webhook
+
+http://76579c3f.ngrok.com
---
+++ app.js
@@ -0,0 +1,21 @@
+// Load the http module to create an http server.
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+
+app.use(bodyParser.json())
+
+var server = app.listen(8081);
+
+app.post('/', function(req, res) {
+   // print to console
+   console.log(req.body);
+
+   // just call res.end(), or show as string on web
+   //res.send(JSON.stringify(req.body, null, 4));
+   res.writeHead(200, {"Content-Type": "text/plain"});
+   res.end();
+});
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8081/");
